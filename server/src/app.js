831a3546a2b8a32ff89d6790fcd2d6060a0b3cb8
Devraj Singhal <devrajsinghal61@gmail.com>
2026-08-24 23:16:33 +0530

    Allow Idempotency-Key in CORS headers

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ export const createApp = () => {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Idempotency-Key');
     if (req.method === 'OPTIONS') {
       return res.sendStatus(200);
     }
